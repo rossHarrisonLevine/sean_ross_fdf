770ecb5baeb91fb456ce1a0e876cb4783ac8bc71
seanjones2848 <seanjones2848@gmail.com>
2017-11-08 15:50:18 -0800

    changed center back to min

--- union/src/put_to_center.c
+++ union/src/put_to_center.c
@@ -6,7 +6,7 @@
 /*   By: rlevine <rlevine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/06 09:34:37 by sjones            #+#    #+#             */
-/*   Updated: 2017/11/08 15:24:36 by sjones           ###   ########.fr       */
+/*   Updated: 2017/11/08 15:49:59 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	put_to_center(t_super *s)
 {
-	zoom(0.8 * (MAX(s->win->w / s->map->w, s->win->h / s->map->h)), s->map);
+	zoom(0.8 * (MIN(s->win->w / s->map->w, s->win->h / s->map->h)), s->map);
 	rotate('z', 1, PI, s->map);
 	rotate('y', -1, 5 * PI / 6, s->map);
 	rotate('x', -1, PI / 6, s->map);
